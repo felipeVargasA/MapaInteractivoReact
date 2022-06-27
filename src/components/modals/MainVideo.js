e2110804fc5df3bc7933db2eab6cbb6066c3515f
Robert Hernnadez <rahernande96@gmail.com>
2022-06-27 19:36:37 -0400

    fix

--- src/components/modals/MainVideo.js
+++ src/components/modals/MainVideo.js
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 
 const MainVideo = () => {
+	const [openModal, setOpenModal] = useState(true);
 
-    const [openModal, setOpenModal] = useState(true);
-
-	return (openModal &&
-		<>
-			<div className="bg-black opacity-40 fixed inset-0 z-50"></div>
-			<div
-				className="justify-center items-center flex fixed inset-0 z-50"
-				onClick={() => setOpenModal(false)}
-			>
+	return (
+		openModal && (
+			<>
+				<div className="bg-black opacity-40 fixed inset-0 z-50"></div>
 				<div
-					className="rounded-lg w-[80%] bg-white"
-					onClick={(e) => {
-						e.stopPropagation();
-					}}
+					className="justify-center items-center flex fixed inset-0 z-50"
+					onClick={() => setOpenModal(false)}
 				>
+					
+						<video width="80%" height="100%" controls autoPlay muted onClick={(e) => {
+							e.stopPropagation();
+						}}>
+                            <source src="/videos/intro-skyview.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
 
-                    <video width="100%" height="100%" controls autoPlay muted>
-                        <source src="/videos/intro-skyview.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                    </video>
-
-                </div>
-			</div>
-			<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-		</>
+					
+				</div>
+				<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+			</>
+		)
 	);
 };
 
